53feeb1f8648d055f06fc7dfb330674c646f8452
Doruk Özer <dorukozer@protonmail.com>
2025-01-19 23:46:34 +0300

    feat: bigquery and drizzle, postgresql sdk setup
    - tested bigquery on hello router

---
+++ src/db/index.ts
@@ -0,0 +1,31 @@
+import { join } from 'path';
+import { homedir } from 'os';
+
+import { BigQuery } from '@google-cloud/bigquery';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+const BIGQUERY_PROJECT_ID = process.env.BIGQUERY_PROJECT_ID;
+const BIGQUERY_CREDENTIALS_JSON = process.env.BIGQUERY_CREDENTIALS_JSON;
+const POSTGRESQL_DATABASE_URL = process.env.POSTGRESQL_DATABASE_URL;
+
+if (
+  !BIGQUERY_PROJECT_ID ||
+  !BIGQUERY_CREDENTIALS_JSON ||
+  !POSTGRESQL_DATABASE_URL
+) {
+  throw new Error('Missing env vars');
+}
+
+const bq = new BigQuery({
+  projectId: process.env.BIGQUERY_PROJECT_ID,
+  keyFilename: join(homedir(), BIGQUERY_CREDENTIALS_JSON)
+});
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+const db = drizzle(pool);
+
+export { bq, db };
--- src/routers/hello.ts
+++ src/routers/hello.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
+
 import { router, publicProcedure } from '../trpc';
+import { bq } from '../db';
 
 export const helloRouter = router({
   greeting: publicProcedure
@@ -10,5 +12,24 @@ export const helloRouter = router({
     )
     .query(async ({ input }) => ({
       greeting: `Hello ${input?.name ?? 'world'}!`
-    }))
+    })),
+  test: publicProcedure.query(async () => {
+    const [tables] = await bq
+      .dataset(process.env.BIGQUERY_DATASET_NAME ?? '')
+      .getTables();
+
+    const tableDetails = tables
+      // When I filter by id includes event, I saw some tables with events_intraday_<date> to ignore them I moved to filtering them by length
+      // .filter((table) => table.id.startsWith('events'))
+      .filter((table) => table.id?.length == 15)
+      .map((table) => ({
+        tableId: table.id,
+        type: table.metadata.type,
+        creationTime: table.metadata.creationTime
+      }));
+
+    return {
+      tableDetails
+    };
+  })
 });
