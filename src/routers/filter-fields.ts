10073ab126635110ee6f68ed53ffef56ac9b501d
Doruk Özer <dorukozer@protonmail.com>
2025-01-25 06:08:59 +0300

    feat: get min and max dates for filtering

--- src/routers/filter-fields.ts
+++ src/routers/filter-fields.ts
@@ -1,7 +1,7 @@
 import { sql } from 'drizzle-orm';
 
 import { router, publicProcedure } from '../trpc';
-import { users } from '../db/schemas';
+import { users, sessions } from '../db/schemas';
 import { db } from '../db';
 
 export const filterFieldsRouter = router({
@@ -18,5 +18,18 @@ export const filterFieldsRouter = router({
       platforms: platformResult.rows.map((row) => row.platform),
       countries: countryResult.rows.map((row) => row.country)
     } as { platforms: string[]; countries: string[] };
+  }),
+  getSessionDateRange: publicProcedure.query(async () => {
+    const result = await db.execute(
+      sql`SELECT
+        MIN(session_date) as earliest_date,
+        MAX(session_date) as latest_date
+        FROM ${sessions}`
+    );
+
+    return {
+      earliestDate: result.rows[0].earliest_date,
+      latestDate: result.rows[0].latest_date
+    } as { earliestDate: Date; latestDate: Date };
   })
 });
